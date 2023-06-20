72b320369cff4482a8ef896909d5897e3b6c3e69
Faraz <111528872+Faraz-Ghani@users.noreply.github.com>
2023-06-20 10:26:10 +0500

    Created Factorial.java (Task 5)

--- Java/Factorial.java
+++ Java/Factorial.java
@@ -4,3 +4,23 @@
 // Use a loop (for or while) to calculate the factorial of the number.
 // Multiply the current number by the factorial value and update it in each iteration.
 // Print the factorial value.
+
+import java.util.Scanner;
+
+public class Factorial{
+    public static void main(String[] args) {
+     System.out.print("Please Enter the number you want the factorial of : "); 
+     Scanner sc = new Scanner(System.in);
+     int n = sc.nextInt();
+     System.out.println("The factorial of " + n + " is " + factorial(n));
+    }   
+
+    public static int factorial(int n){
+        int factorial = 1;
+        while(n>0){
+           factorial=factorial*n;
+           n--;
+        }
+        return factorial;
+    }
+}
