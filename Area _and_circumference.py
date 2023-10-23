93348fe3616f9897b406faeed4e219fe9a20ebe4
solo <53935554+0xS0l0@users.noreply.github.com>
2023-10-23 07:21:28 +0530

    Add files via upload
    Python

---
+++ Area _and_circumference.py
@@ -0,0 +1,17 @@
+# program to calculate area and circumference
+# Start of the program
+# importing math module
+from math import pi
+
+# getting input
+radius = float(input("Enter the radius of the circle: "))
+
+# formula
+area = pi * radius * radius
+circumference = 2 * pi * radius
+
+# printing output
+print("\nThe area of the circle is =",area)
+print("\nThe circumference of the circle is =",circumference)
+
+# End of the program
